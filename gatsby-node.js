be585551b60e3835eb17d33be1c47b4b52f2adf9
RaidenDilan <raiden18@me.com>
2020-09-22 14:48:05 +0100

    Building our Blog P.3

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -12,4 +13,35 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
       value: slug
     })
   }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  // graphql query can be used as graphql`` OR graphql(``)
+  // Because of node doesn't come with ES6 feature right at the jump so we pass the parentheses just to be safe.
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  .then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          slug: node.fields.slug // pass slug value here
+        }
+      })
+    })
+  })
 }
