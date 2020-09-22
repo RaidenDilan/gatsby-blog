6759ae393d8b835d5ccbd1d373048055ac59ef7d
RaidenDilan <raiden18@me.com>
2020-09-22 14:59:51 +0100

    Building our Blog Final

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,45 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
-
+import styled from "styled-components"
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+
+const Title = styled.h1`
+  display: inline-block;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+
+  &:hover {
+    color: #1dcaff;
+  }
+`
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`
+
+const BlogBody = styled.div`
+  margin-bottom: 50px;
+`
 
 export default ({ data }) => {
-  console.log(data);
   return (
     <Layout>
-      <SEO title="Home" />
       <div>
-        <h1>Raiden's Thoughts</h1>
-        <h4>{ data.allMarkdownRemark.totalCount }</h4>
-        {
-          data.allMarkdownRemark.edges.map(({ node }) => (
-            <div key={ node.id }>
-              <span>{ node.frontmatter.title } - { node.frontmatter.date }</span>
-              <p>{ node.excerpt }</p>
-            </div>
-          ))
-        }
+        <Title>Thoughts by Yihua</Title>
+        <h4>{ data.allMarkdownRemark.totalCount } Posts</h4>
+        { data.allMarkdownRemark.edges.map(({ node }) => (
+          <BlogBody key={ node.id }>
+            <BlogLink to={ node.fields.slug }>
+              <BlogTitle>
+                { node.frontmatter.title} <span>— { node.frontmatter.date }</span>
+              </BlogTitle>
+            </BlogLink>
+            <p>{ node.frontmatter.description || node.excerpt }</p>
+          </BlogBody>
+        )) }
       </div>
     </Layout>
   )
@@ -28,20 +47,22 @@ export default ({ data }) => {
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
       edges {
         node {
           id
           frontmatter {
-            description
             title
-            date
+            date(formatString: "DD MMMM, YYYY")
+            description
           }
-          html
-          excerpt
+          fields {
+            slug
+          }
+          excerpt(truncate: true)
         }
       }
-      totalCount
     }
   }
 `
